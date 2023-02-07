b71869f0b0a0871b926955ae595cab742a2ae1df
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-02-07 07:59:08 +0300

    fix error

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,23 +5,23 @@ const AboutPage = () => {
         <div className={styles.main}>
             <h1 className={styles.title}> About Vacation Hub </h1>
             <p className={styles.text}>
-            At Vacation Hub, we believe that travel is one of life's greatest pleasures. Our mission is to make travel accessible, affordable, and enjoyable for everyone. We are a team of passionate travel experts who are dedicated to providing our customers with the best possible travel experiences. Whether you're looking for a romantic getaway, a family vacation, or an adrenaline-fueled adventure, we have the expertise and resources to make it happen.
+            At Vacation Hub, we believe that travel is one of life&#39;s greatest pleasures. Our mission is to make travel accessible, affordable, and enjoyable for everyone. We are a team of passionate travel experts who are dedicated to providing our customers with the best possible travel experiences. Whether you&#39;re looking for a romantic getaway, a family vacation, or an adrenaline-fueled adventure, we have the expertise and resources to make it happen.
             </p>
 
             <p className={styles.text}>
-            Founded in 1999, Vacation Hub has established itself as a leading tour and travel company, offering a wide range of travel services to customers all over the world. From flights and hotels to tours and activities, we have everything you need to create the perfect vacation. Our team of travel advisors has years of experience and is well-versed in the latest travel trends and destinations, so you can be confident that you're getting the best advice and value for your money.
+            Founded in 1999, Vacation Hub has established itself as a leading tour and travel company, offering a wide range of travel services to customers all over the world. From flights and hotels to tours and activities, we have everything you need to create the perfect vacation. Our team of travel advisors has years of experience and is well-versed in the latest travel trends and destinations, so you can be confident that you&#39;re getting the best advice and value for your money.
             </p>
 
             <p className={styles.text}>
-            At Vacation Hub, we understand that every traveler is unique, and that's why we offer personalized travel solutions to meet the needs of each and every one of our customers. Whether you're looking for a tailor-made itinerary or a pre-packaged tour, our travel advisors will work with you to create a travel experience that is tailored to your interests, budget, and schedule. From start to finish, we're here to make your travel experience as seamless and enjoyable as possible.
+            At Vacation Hub, we understand that every traveler is unique, and that&#39;s why we offer personalized travel solutions to meet the needs of each and every one of our customers. Whether you&#39;re looking for a tailor-made itinerary or a pre-packaged tour, our travel advisors will work with you to create a travel experience that is tailored to your interests, budget, and schedule. From start to finish, we&#39;re here to make your travel experience as seamless and enjoyable as possible.
             </p>
 
             <p className={styles.text}>
-            We are proud to offer our customers the very best in travel services, and we are constantly striving to improve our offerings and expand our reach. We believe that travel should be accessible to everyone, regardless of budget, and we work hard to provide our customers with exceptional value for money. Whether you're a seasoned traveler or a first-timer, we're here to help you plan your next adventure.
+            We are proud to offer our customers the very best in travel services, and we are constantly striving to improve our offerings and expand our reach. We believe that travel should be accessible to everyone, regardless of budget, and we work hard to provide our customers with exceptional value for money. Whether you&#39;re a seasoned traveler or a first-timer, we&#39;re here to help you plan your next adventure.
             </p>
 
             <p className={styles.text}>
-            At Vacation Hub, we're more than just a tour and travel company. We're a community of travelers and travel enthusiasts who are passionate about exploring new destinations and creating unforgettable memories. We invite you to join us on this journey, and we look forward to helping you plan your next adventure. Whether you're seeking adventure, relaxation, or something in between, we're here to help you turn your travel dreams into a reality.    
+            At Vacation Hub, we&#39;re more than just a tour and travel company. We&#39;re a community of travelers and travel enthusiasts who are passionate about exploring new destinations and creating unforgettable memories. We invite you to join us on this journey, and we look forward to helping you plan your next adventure. Whether you&#39;re seeking adventure, relaxation, or something in between, we&#39;re here to help you turn your travel dreams into a reality.    
             </p>
 
         </div>
